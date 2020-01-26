44c8f23eddf4b9d4bc07835bcb0fc7a9a3639000
Anthony Quijano <anthony@quijano.net>
2020-01-26 13:25:48 -0800

    Update the DOM by changing the state of the app

--- src/App.js
+++ src/App.js
@@ -3,14 +3,35 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Anthony', age: 23 },
+      { name: 'Taylor', age: 24 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    // console.log('Was clicked!');
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+    this.setState( {
+      persons: [
+        { name: 'Maximilian', age: 28 },
+        { name: 'Anthony', age: 23 },
+        { name: 'Taylor', age: 25 }
+      ]
+    } )
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hello Pumpkin</h1>
         <p>This is really working.</p>
-        <Person name="Anthony" age="23">My Hobbies: Programming</Person>
-        <Person name="Taylor" age="24"/>
-        <Person name="Max" age="28"/>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobbies: Programming</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does thsi work now?'));
