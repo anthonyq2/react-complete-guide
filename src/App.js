2ab00687fa912f98c12b7f9bfdd238d804f91425
Anthony Quijano <anthony@quijano.net>
2020-02-16 17:10:57 -0800

    Cleanup rendering logic: move Persons div to a variable

--- src/App.js
+++ src/App.js
@@ -48,6 +48,26 @@ class App extends Component {
       cursor: 'pointer'
     }
 
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person 
+            name={this.state.persons[0].name} 
+            age={this.state.persons[0].age}
+            click={this.switchNameHandler.bind(this, 'Anthony!')} >My Hobbies: Programming
+          </Person>
+          <Person 
+            name={this.state.persons[1].name} 
+            age={this.state.persons[1].age} 
+            changed={this.nameChangedHandler} />
+          <Person 
+            name={this.state.persons[2].name} 
+            age={this.state.persons[2].age} />
+        </div>
+      )
+    }
+
     return (
       <div className="App">
         <h1>Hello Pumpkin</h1>
@@ -56,23 +76,7 @@ class App extends Component {
           style={style}
           onClick={this.togglePersonsHandler}>Switch Name
         </button>
-        { 
-          this.state.showPersons ?
-            <div>
-              <Person 
-                name={this.state.persons[0].name} 
-                age={this.state.persons[0].age}
-                click={this.switchNameHandler.bind(this, 'Anthony!')} >My Hobbies: Programming
-              </Person>
-              <Person 
-                name={this.state.persons[1].name} 
-                age={this.state.persons[1].age} 
-                changed={this.nameChangedHandler} />
-              <Person 
-                name={this.state.persons[2].name} 
-                age={this.state.persons[2].age} />
-            </div> : null
-        }
+        {persons}
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does thsi work now?'));
